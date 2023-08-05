caef1a7ad3497d58316cae11710d065024f53bd1
FlaminiaCastano <flaminiacastano@gmail.com>
2023-08-05 15:57:19 -0300

    Create Song entity and ListSongViewModel

---
+++ SwiftUILearning/SwiftUILearning/Module/TaylorSwiftSongsList/Entity/ListSongViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ListSongViewModel.swift
+//  SwiftUILearning
+//
+//  Created by Flaminia Castaño on 05/08/2023.
+//
+
+import Foundation
+
+struct ListSongViewModel {
+    var groupedSongs: [String: [Song]] = ["": []]
+}
